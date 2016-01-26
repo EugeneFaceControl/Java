141326fca9aa3fdea896b47d285b317cd9665845
Euegene <eugenefacecontrol@gmail.com>
2016-01-26 12:30:30 +0300

    26.01.2016

---
+++ 3A/student/Matrix.java
@@ -0,0 +1,19 @@
+package student;
+import student.Student;
+
+public class Matrix {
+	private String[] studentList;
+	
+	Student s = new Student();
+	String v = s.getFaculty();
+	
+	
+
+	public String[] getStudentList() {
+		return studentList;
+	}
+
+//	public void setStudentList(String[] studentList) {
+//		this.studentList = studentList;
+//	}
+}
---
+++ 3A/student/Student.java
@@ -0,0 +1,87 @@
+package student;
+
+public class Student {
+	private int id, dateOfBirth, add, phone, course, group;
+	private String surname, name, faculty, patronymic; // מעקוסעגמ
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPatronymic() {
+		return patronymic;
+	}
+
+	public void setPatronymic(String patronymic) {
+		this.patronymic = patronymic;
+	}
+
+	public int getDateOfBirth() {
+		return dateOfBirth;
+	}
+
+	public void setDateOfBirth(int dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+
+	public int getAdd() {
+		return add;
+	}
+
+	public void setAdd(int add) {
+		this.add = add;
+	}
+
+	public int getPhone() {
+		return phone;
+	}
+
+	public void setPhone(int phone) {
+		this.phone = phone;
+	}
+
+	public String getFaculty() {
+		return faculty;
+	}
+
+	public void setFaculty(String faculty) {
+		this.faculty = faculty;
+	}
+
+	public int getCourse() {
+		return course;
+	}
+
+	public void setCourse(int course) {
+		this.course = course;
+	}
+
+	public int getGroup() {
+		return group;
+	}
+
+	public void setGroup(int group) {
+		this.group = group;
+	}
+
+}
